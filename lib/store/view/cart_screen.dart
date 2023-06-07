66eed6c6cde545de8598987b2dfe16d27d7a7e68
Amin <amin.arshadinia@whipmobility.com>
2023-06-07 13:12:43 +0800

    feat: render items in gridview and handle empty cart

--- lib/store/view/cart_screen.dart
+++ lib/store/view/cart_screen.dart
@@ -7,6 +7,8 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final hasEmptyCart =
+        context.select<StoreBloc, bool>((b) => b.state.cartIds.isEmpty);
     final cartProducts = context.select<StoreBloc, List<Product>>(
       (b) => b.state.products
           .where((product) => b.state.cartIds.contains(product.id))
@@ -17,7 +19,88 @@ class CartScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('My Cart'),
       ),
-      body: Text('${cartProducts.length}'),
+      body: hasEmptyCart
+          ? emptyCartContainer(context)
+          : GridView.builder(
+              padding: const EdgeInsets.all(10),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3),
+              itemCount: cartProducts.length,
+              itemBuilder: (context, index) {
+                final product = cartProducts[index];
+                return Card(
+                  key: ValueKey(product.id),
+                  child: Column(
+                    children: [
+                      Flexible(
+                        child: Image.network(product.image),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Expanded(
+                        child: Text(
+                          product.title,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 4,
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: OutlinedButton(
+                            onPressed: () => context
+                                .read<StoreBloc>()
+                                .add(StoreProductsRemovedFromCart(product.id)),
+                            style: const ButtonStyle(
+                              padding: MaterialStatePropertyAll(
+                                EdgeInsets.all(10),
+                              ),
+                              backgroundColor: MaterialStatePropertyAll<Color>(
+                                  Colors.black12),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  Icon(Icons.remove_shopping_cart),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text('Remove from cart'),
+                                ],
+                              ),
+                            )),
+                      )
+                    ],
+                  ),
+                );
+              }),
+    );
+  }
+
+  Center emptyCartContainer(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Your cart is empty'),
+          const SizedBox(
+            height: 10,
+          ),
+          OutlinedButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Add product'),
+          ),
+        ],
+      ),
     );
   }
 }
