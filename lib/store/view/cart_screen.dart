7ee798fb6915ce40fdce31ac63f6ab6e369d6ce4
Amin <amin.arshadinia@whipmobility.com>
2023-06-07 12:29:28 +0800

    feat: get products and extract selected ones

--- lib/store/view/cart_screen.dart
+++ lib/store/view/cart_screen.dart
@@ -1,14 +1,23 @@
+import 'package:bloc_app_test/store/store.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final cartProducts = context.select<StoreBloc, List<Product>>(
+      (b) => b.state.products
+          .where((product) => b.state.cartIds.contains(product.id))
+          .toList(),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Cart'),
       ),
+      body: Text('${cartProducts.length}'),
     );
   }
 }
