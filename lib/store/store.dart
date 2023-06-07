cefc620ad1389b0bd70c76d457685629bc748e38
Amin <amin.arshadinia@whipmobility.com>
2023-06-07 11:55:03 +0800

    feat: export cart screen file

--- lib/store/store.dart
+++ lib/store/store.dart
@@ -3,3 +3,4 @@ export 'bloc/store_event.dart';
 export 'bloc/store_state.dart';
 export 'package:bloc_app_test/store/models/product.dart';
 export 'package:bloc_app_test/store/view/store_app.dart';
+export 'package:bloc_app_test/store/view/cart_screen.dart';
