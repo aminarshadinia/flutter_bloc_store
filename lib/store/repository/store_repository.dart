978d7ecc9af75475400d16d225c4310cc12c387b
Amin <amin.arshadinia@whipmobility.com>
2023-06-07 10:25:25 +0800

    feat: create store repository file

---
+++ lib/store/repository/store_repository.dart
@@ -0,0 +1,22 @@
+import 'package:bloc_app_test/store/models/product.dart';
+import 'package:dio/dio.dart';
+
+class StoreRepository {
+  final Dio _client = Dio(
+    BaseOptions(
+      baseUrl: 'https://fakestoreapi.com/products',
+    ),
+  );
+  Future<List<Product>> getProducts() async {
+    final response = await _client.get('');
+    return (response.data as List)
+        .map(
+          (json) => Product(
+            id: json['id'],
+            title: json['title'],
+            image: json['image'],
+          ),
+        )
+        .toList();
+  }
+}
