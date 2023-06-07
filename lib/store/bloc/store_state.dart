0d7dd72e30c728f85dd6391cdbfeea46974fda10
Amin <amin.arshadinia@whipmobility.com>
2023-06-07 10:40:24 +0800

    feat: create store state file

--- lib/store/bloc/store_state.dart
+++ lib/store/bloc/store_state.dart
@@ -1,6 +1,37 @@
-part of 'store_bloc.dart';
+import 'package:bloc_app_test/store/store.dart';
 
-@immutable
-abstract class StoreState {}
+enum StoreRequest {
+  unknown,
+  requestInProgress,
+  requestSuccess,
+  requestFailure,
+}
 
-class StoreInitial extends StoreState {}
+class StoreState {
+  const StoreState({
+    this.products = const [],
+    this.productsStatus = StoreRequest.unknown,
+    this.cartIds = const {},
+  });
+  final List<Product> products;
+  final StoreRequest productsStatus;
+  final Set<int> cartIds;
+
+  /*
+because our state is immutable we define a copywith method which is responsibe for
+ creating a new store state object which accept the updated state fields or it will
+ persist the previous state values if they have not changed
+}
+*/
+
+  StoreState copyWith({
+    final List<Product>? products,
+    final StoreRequest? productsStatus,
+    final Set<int>? cartIds,
+  }) =>
+      StoreState(
+        products: products ?? this.products,
+        productsStatus: productsStatus ?? this.productsStatus,
+        cartIds: cartIds ?? this.cartIds,
+      );
+}
