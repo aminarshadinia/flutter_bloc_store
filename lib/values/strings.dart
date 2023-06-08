60c48277f0c72aa1e092800b1706752425750b9f
Amin <amin.arshadinia@whipmobility.com>
2023-06-08 12:57:18 +0800

    feat: add string consts

---
+++ lib/values/strings.dart
@@ -0,0 +1,15 @@
+class Strings {
+  static const flutterDemo = 'Flutter Demo';
+  static const myCart = 'My Cart';
+  static const myStore = 'My Store';
+  static const shoppingCartUpdated = 'Shopping cart updated';
+  static const problemLoadingProducts = 'Problem loading products';
+  static const tryAgain = 'Try again';
+  static const noProductsToView = 'No products to view';
+  static const loadProducts = 'Load Products';
+  static const removeFromCart = 'Remove from cart';
+  static const addToCart = 'Add to cart';
+  static const viewCart = 'View Cart';
+  static const yourCartIsEmpty = 'Your cart is empty';
+  static const addProduct = 'Add product';
+}
